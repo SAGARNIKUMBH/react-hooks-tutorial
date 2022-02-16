193352cfa371da0b022c3fce2ab426bd23c221dd
SAGAR_NIKUMBH <sagarnikumbh.weoto1991@gmail.com>
2022-02-16 14:31:50 +0530

    UseEffect Part Done-16-02-2022

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import HookCounter from "./Components/HookCounter";
 import HookCounterFour from "./Components/HookCounterFour";
+import HookCounterOne from "./Components/HookCounterOne";
 import HookCounterThree from "./Components/HookCounterThree";
 import HookCounterTwo from "./Components/HookCounterTwo";
+import HookMouse from "./Components/HookMouse";
+import IntervalHookCounter from "./Components/IntervalHookCounter";
+import MouseContainer from "./Components/MouseContainer";
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour />
+      <IntervalHookCounter />
+      {/* <MouseContainer /> */}
+      {/* <HookMouse /> */}
+      {/* <HookCounterOne /> */}
+      {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounter /> */}
